f378827d44709ebfc06f393a7a40413e1c66a840
zoryon <gioelespata@gmail.com>
2025-08-11 21:53:51 +0200

    added: chess types

---
+++ src/constants/index.ts
@@ -0,0 +1,2 @@
+export { PIECES } from "@/constants/pieces-constants";
+export { BOARD } from "@/constants/board-constants";
---
+++ src/types/index.ts
@@ -0,0 +1,2 @@
+export type { PieceType, PieceInstance, Color, Ability } from "@/types/pieces-types";
+export type { BoardCell, BoardState } from "@/types/board-types";
