a00010c25f404f2ce70a8e2b1deee968dcc4f3e5
zoryon <gioelespata@gmail.com>
2025-08-12 01:05:09 +0200

    fix security issue

--- src/lib/jwt.ts
+++ src/lib/jwt.ts
@@ -64,7 +64,7 @@ export async function setRefreshTokenCookie({
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
         sameSite: "strict",
-        path: "/",
+        path: "/refresh",
         expires: expiresAt
     });
 
